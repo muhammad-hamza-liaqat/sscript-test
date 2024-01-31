a624f05320fe1d1e737f16dc2eb94fe91f28df16
Muhammad Hamza <mh408800@gmail.com>
2024-02-01 03:30:58 +0500

    adding comment

--- controller/userController.js
+++ controller/userController.js
@@ -4,9 +4,9 @@ const addUser = async (req, res) => {
   //   res.end("hello");
 
   const newUser = req.body;
-//   joi validations to validate the data in the req.body
+  //   joi validations to validate the data in the req.body
   const validationErr = userValidations(newUser);
-//   if there is any error then,
+  //   if there is any error then,
   if (validationErr.length > 0) {
     return res.status(400).json({ status: 400, error: validationErr });
   }
@@ -14,13 +14,11 @@ const addUser = async (req, res) => {
     const newUserToAdd = await userModel.create(newUser);
     console.log("user created successfully", newUserToAdd);
     // res in json as per requirement
-    return res
-      .status(201)
-      .json({
-        status: 201,
-        message: "user created successfully!",
-        data: newUserToAdd,
-      });
+    return res.status(201).json({
+      status: 201,
+      message: "user created successfully!",
+      data: newUserToAdd,
+    });
   } catch (error) {
     console.log("internal server error-adduser", error);
     return res.status(500).json({ status: 500, error: error });
