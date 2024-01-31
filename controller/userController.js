1b0818a08aeb7531d75f6d1223ed03220d54eeb1
Muhammad Hamza <mh408800@gmail.com>
2024-02-01 03:30:27 +0500

    code comment

--- controller/userController.js
+++ controller/userController.js
@@ -4,13 +4,16 @@ const addUser = async (req, res) => {
   //   res.end("hello");
 
   const newUser = req.body;
+//   joi validations to validate the data in the req.body
   const validationErr = userValidations(newUser);
+//   if there is any error then,
   if (validationErr.length > 0) {
     return res.status(400).json({ status: 400, error: validationErr });
   }
   try {
     const newUserToAdd = await userModel.create(newUser);
     console.log("user created successfully", newUserToAdd);
+    // res in json as per requirement
     return res
       .status(201)
       .json({
